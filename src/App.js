f6a0c348d42031045f25d665c0aa1afa70fb6294
Stanislav Pronkin <stanley_@mail.ru>
2022-05-30 01:29:37 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Layout from './components/Layout';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+import NotFound from './pages/404';
+import CatalogPage from './pages/CatalogPage';
+import Main from './pages/Main';
+import ProductView from './pages/ProductView';
+import Cart from './pages/Cart';
+import { restoreCart } from './actions/actionCreators';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(restoreCart())
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route exact path='/about' element={<About />} />
+            <Route exact path='/catalog/:id' element={<ProductView />} />
+            <Route exact path='/catalog' element={<CatalogPage />} />
+            <Route exact path='/contacts' element={<Contacts />} />
+            <Route exact path='/cart' element={<Cart />} />
+            <Route exact path='/404' element={<NotFound />} />
+            <Route exact path='/' element={<Main />} />
+            <Route path='*' element={<Navigate to={'/404'} />} />
+          </Routes>
+        </Layout>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//       <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store} >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
